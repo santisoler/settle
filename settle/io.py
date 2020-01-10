11961dd7e67ae4a91e03f95fc086a550c0b33d53
Santiago Soler <santiago.r.soler@gmail.com>
2020-01-10 14:45:05 -0300

    Move check defaults packages func to io.py
    Remove utils.py

--- settle/io.py
+++ settle/io.py
@@ -2,7 +2,6 @@
 Functions to read and save files
 """
 import yaml
-from .utils import check_valid_default_packages
 
 
 def read_packages_yaml(packages_yaml):
@@ -14,5 +13,23 @@ def read_packages_yaml(packages_yaml):
     default_packages = []
     if "default" in packages:
         default_packages = packages.pop("default")
-        check_valid_default_packages(packages, default_packages)
+        _check_valid_default_packages(packages, default_packages)
     return packages, default_packages
+
+
+def _check_valid_default_packages(packages, default):
+    "Check if the groups in default are valid"
+    missing_groups = [group for group in default if group not in packages]
+    if missing_groups:
+        if len(missing_groups) == 1:
+            plural = ""
+            missing_groups = missing_groups[0]
+        else:
+            plural = "s"
+            missing_groups = "', '".join(missing_groups)
+        raise ValueError(
+            "Group{0} '{1}' not recognized as valid package group{0}".format(
+                plural, missing_groups
+            )
+            + " inside the passed YAML package file."
+        )
